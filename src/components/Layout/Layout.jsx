9cf4e4efd9a8a574f8fed431df1635f4ebfb5904
Elina <elina@Mac-mini-Elina.local>
2023-08-16 12:46:52 +0300

    media queries

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Footer/Footer';
+import  logo  from '../../images/logo.png'
 import './Layout.scss';
 
 const Layout = () => {
 
   return (
     <>
+    <header className="header">
+      <img src={logo} alt="logo" className="logo"/>
+    </header>
       <main className="layout">
         <Outlet />
       </main>
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -4,14 +4,14 @@ import './Resume.scss';
 
 const Resume = () => {
   return (
-   <div className="container resume">
+   <div className="container">
       <div className="photo" />
-      <h1>Elina Razina</h1>
-      <h2>Frontend developer</h2>
+      <h1 className='header-1'>Elina Razina</h1>
+      <h2 className='header-2'>Frontend developer</h2>
       <div className="location">Saint-Petersburg, Russia</div>
       <a href="./Elina_Razina_CV.pdf" download className="link-cv" aria-label="Download CV">Download CV</a>
 
-      <h2>Contact me</h2>
+      <h2 className='header-2'>Contact me</h2>
       <div className="social">
         <a href="https://t.me/Elina_Razina" className="social-link social-link-telegram" target="_blank" rel="noopener noreferrer" aria-label="Telegram"><span style={{display: 'none'}}>Telegram link</span></a>
         <a href="https://wa.me/79112548531" className="social-link social-link-whatsapp" target="_blank" rel="noopener noreferrer" aria-label="WhatsApp"><span style={{display: 'none'}}>WhatsApp link</span></a>
@@ -20,18 +20,18 @@ const Resume = () => {
      
       <Divider className="divider"/>
 
-      <h2>Skills</h2>
+      <h2 className='header-2'>Skills</h2>
         <div className="skills">
-        <span class="badge skills-badge">HTML</span>
-        <span class="badge skills-badge">CSS</span>
-        <span class="badge skills-badge">SASS</span>
-        <span class="badge skills-badge">REACT</span>
-        <span class="badge skills-badge">JAVASCRIPT</span>
-        <span class="badge skills-badge">GIT</span>
-        <span class="badge skills-badge">FIGMA</span>
-        <span class="badge skills-badge">WEBPACK</span>
-        <span class="badge skills-badge">MOBX</span>
-        <span class="badge skills-badge">RESPONSIVE</span>
+          <span class="badge skills-badge">HTML</span>
+          <span class="badge skills-badge">CSS</span>
+          <span class="badge skills-badge">SASS</span>
+          <span class="badge skills-badge">REACT</span>
+          <span class="badge skills-badge">JAVASCRIPT</span>
+          <span class="badge skills-badge">GIT</span>
+          <span class="badge skills-badge">FIGMA</span>
+          <span class="badge skills-badge">WEBPACK</span>
+          <span class="badge skills-badge">MOBX</span>
+          <span class="badge skills-badge">RESPONSIVE</span>
       </div>
       
     </div>
